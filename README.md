d75059233a53bd88d57bd47d1c20fde3dd3da5c4
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-12-14 02:10:09 +0900

    add: model 추가

--- README.md
+++ README.md
@@ -5,15 +5,31 @@
 
 ### Django 실행 방법
 1. 데이터베이스 설정
-- manage.py 파일이 있는 폴더(프로젝트 최상단)에 my_settings 파일 넣어주기
-- my_settings 파일에는 데이터베이스 정보 등 개인정보를 담기
-   
-2. 모델 적용
-- python manage.py makemigrations
-- python manage.py migrate
+    1) pip install mysqlclient
+    - 라이브러리 설치하기
+    2) touch my_settings
+    - manage.py 파일이 있는 폴더(프로젝트 최상단)에 my_settings 파일 만들어주기
+    3) my_settings 파일에 데이터베이스 정보 넣기
+    ```
+   DATABASES = {   
+        'default': {   
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': '데이터베이스 이름',
+            'USER': '사용자',
+            'PASSWORD': '비밀번호',
+            'HOST': 'localhost',
+            'PORT': '3306'
+        }
+    }
+   ```
+<br>
 
-3. Django 실행
-- python manage.py runserver
+2. 모델 적용하기   
+    1) python manage.py makemigrations   
+    2) python manage.py migrate   
+    
 
-4. 웹페이지 띄우기
-- 웹 브라우저에 http://127.0.0.1:8000/greenz/ 입력하기
+3. `python manage.py runserver` 로 Django 실행
+
+
+4. 웹 브라우저에 `http://127.0.0.1:8000/greenz/` 입력하기
--- greenz/models.py
+++ greenz/models.py
@@ -1,3 +1,150 @@
 from django.db import models
 
-# Create your models here.
+
+class Cart(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'cart'
+
+
+class CartItem(models.Model):
+    id = models.IntegerField(primary_key=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    cart = models.ForeignKey(Cart, models.DO_NOTHING)
+    product = models.ForeignKey('Product', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'cart_item'
+
+
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+
+
+class Orders(models.Model):
+    id = models.IntegerField(primary_key=True)
+    shipped_date = models.CharField(max_length=135, blank=True, null=True)
+    created_date = models.CharField(max_length=135, blank=True, null=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    cart = models.ForeignKey(Cart, models.DO_NOTHING)
+    status = models.ForeignKey('Status', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'orders'
+
+
+class Package(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'package'
+
+
+class Payment(models.Model):
+    id = models.IntegerField(primary_key=True)
+    total = models.FloatField(blank=True, null=True)
+    payment_time = models.CharField(max_length=135, blank=True, null=True)
+    order = models.OneToOneField(Orders, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'payment'
+
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    cost = models.FloatField(blank=True, null=True)
+    unit = models.IntegerField(blank=True, null=True)
+    weight = models.CharField(max_length=45, blank=True, null=True)
+    origin = models.CharField(max_length=135, blank=True, null=True)
+    expirationdate = models.CharField(db_column='expirationDate', max_length=135, blank=True, null=True)  # Field name made lowercase.
+    keyword = models.CharField(max_length=135, blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
+    category = models.ForeignKey(Category, models.DO_NOTHING)
+    package = models.ForeignKey(Package, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+
+
+class RAddress(models.Model):
+    address = models.CharField(primary_key=True, max_length=135)
+    latitude = models.CharField(max_length=45, blank=True, null=True)
+    longtitude = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'r_address'
+
+
+class Recipe(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    method = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'recipe'
+
+
+class RecipeIngredient(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    weight = models.CharField(max_length=45, blank=True, null=True)
+    recipe = models.ForeignKey(Recipe, models.DO_NOTHING)
+    product = models.ForeignKey(Product, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'recipe_ingredient'
+
+
+class Restaurant(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    address = models.CharField(max_length=135, blank=True, null=True)
+    keyword = models.CharField(max_length=135, blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'restaurant'
+
+
+class Status(models.Model):
+    id = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'status'
+
+
+class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=45)
+    password = models.CharField(max_length=45, blank=True, null=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    email = models.CharField(max_length=90, blank=True, null=True)
+    address = models.CharField(max_length=135, blank=True, null=True)
+    phone_number = models.CharField(max_length=135, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
